79392746836c0808e0652b3b1774109bf15e0c5e
FywOo02 <zhushenzhe0531@gmail.com>
2024-04-14 22:54:39 -0400

    updated the algorithms.py

--- fastApiProject/resources/config.py
+++ fastApiProject/resources/config.py
@@ -1,9 +1,13 @@
 # Credential of google cloud
-CREDENTIAL_PATH = "D:\\ComputerScience\\Python\\job_recommendation_sys\\job_recommender_sys\\fastApiProject\\resources\\credentials\\csm-job-recommender-system-cf5f44bea942.json"
+CREDENTIAL_PATH = "E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\credentials\\csm-job-recommender-system-cf5f44bea942.json"
 
-RESUME_PATH = 'D:\\ComputerScience\\Python\\job_recommendation_sys\\job_recommender_sys\\fastApiProject\\resources\\storage\\resume.pdf'
-SUBJECT_LIST = 'D:\\ComputerScience\\Python\\job_recommendation_sys\\job_recommender_sys\\fastApiProject\\resources\\storage\\subject_utsc'
-LOCAL_RESUME_KEYWORDS_BUCKET = "D:\\ComputerScience\\Python\\job_recommendation_sys\\job_recommender_sys\\fastApiProject\\resources\\resume_keywords"
+RESUME_PATH_1 = 'E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\storage\resume_test_1.pdf'
+RESUME_PATH_2 = 'E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\storage\\resume_test_2.pdf'
+RESUME_PATH_3 = 'E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\storage\\resume_test_3.pdf'
+RESUME_PATH_4 = 'E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\storage\\resume_test_4.pdf'
+
+SUBJECT_LIST = 'E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\storage\\subject_utsc'
+LOCAL_RESUME_KEYWORDS_BUCKET = "E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\resume_keywords"
 LOCAL_RESUME_KEYWORDS_NAME = "resume_data.json"
-LOCAL_JOB_KEYWORDS_BUCKET = "D:\\ComputerScience\\Python\\job_recommendation_sys\\job_recommender_sys\\fastApiProject\\resources\\job_keywords"
+LOCAL_JOB_KEYWORDS_BUCKET = "E:\\Computer Science\\Project\\job_recommender_system\\fastApiProject\\resources\\job_keywords"
 LOCAL_JOB_KEYWORDS_NAME = "job_data.json"
